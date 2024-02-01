57c9b94358428051cbb8174bc861a642cbff90cd
Xin Deng <xind1470@gmail.com>
2024-01-31 17:08:57 -0800

    added docstring to is english word

--- caesar_cipher/is_english_word.py
+++ caesar_cipher/is_english_word.py
@@ -4,7 +4,19 @@ from caesar_cipher.corpus_loader import word_list, name_list
 
 
 def count_words(text):
+    """
+    Counts the number of valid English words in a given text.
 
+    The function uses a list of English words and names loaded from the corpus_loader module.
+    It removes non-alphabetic characters from each candidate word and checks if the resulting word
+    or its lowercase version is present in the word_list or name_list.
+
+    Args:
+        text (str): The text in which words are counted.
+
+    Returns:
+        int: The count of valid English words.
+    """
     candidate_words = text.split()
 
     word_count = 0
